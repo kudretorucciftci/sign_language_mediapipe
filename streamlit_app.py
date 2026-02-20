aca7b7ca0690ae85fcccde5a354923e1e73725db
kudretorucciftci <101557917+kudretorucciftci@users.noreply.github.com>
2026-02-20 03:44:10 +0300

    Streamlit Cloud uyumluluğu için gereksinimler ve importlar optimize edildi

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,14 @@ from tensorflow.keras.models import load_model
 import av
 import threading
 
-# MediaPipe bileşenlerini güvenli bir şekilde içe aktar
+# MediaPipe bileşenlerini standart yolla al
 try:
+    mp_hands = mp.solutions.hands
+    mp_draw = mp.solutions.drawing_utils
+except (AttributeError, ImportError):
+    # Eğer üstteki başarısız olursa, manuel olarak solutions'a erişmeyi dene
     import mediapipe.python.solutions.hands as mp_hands
     import mediapipe.python.solutions.drawing_utils as mp_draw
-except ImportError:
-    try:
-        mp_hands = mp.solutions.hands
-        mp_draw = mp.solutions.drawing_utils
-    except AttributeError:
-        # Son çare: solutions üzerinden manuel deneme
-        from mediapipe.python import solutions as mp_solutions
-        mp_hands = mp_solutions.hands
-        mp_draw = mp_solutions.drawing_utils
 
 # Sayfa Ayarları
 st.set_page_config(page_title="AI Sign Language Translator", layout="wide", page_icon="🤟")
